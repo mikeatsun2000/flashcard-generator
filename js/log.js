3123a7f73c1998d344981b4b5b4ab56b8675b406
Mike Grogan <mikeatsun2000@gmail.com>
2017-06-27 18:47:17 -0700

    implement log levels, printStackTrace

--- js/log.js
+++ js/log.js
@@ -1,26 +1,37 @@
 const mainConsole= new nodeConsole.Console(process.stdout, process.stderr);
-
+let defaultLevel = 0;
 module.exports = {
     DEBUG: 0,
     INFO: 1,
     WARN: 2,
-    defaultLevel: 0,
 
     // log message to console
     log: function(mess, level) {
-        if (level && level >= defaultLevel) {
-            mainConsole.log(__filename + '\n\n' + mess);
+        if (!level) {
+            level = 0;
+        }
+
+        if (level >= defaultLevel) {
+            mainConsole.log(__filename + '\n' + mess);
         }  else {
             //TODO
         }  
     },
 
     printStackTrace: function(e, level ) {
-        if (this.level >= level) {
-            mainConsole.log(__filename + '\n\n' +  e.stack);
+        if (!level) {
+            level = 0;
+        }
+
+        if (level >= defaultLevel) {
+            mainConsole.log(__filename + '\n' +  e.stack);
         }  else  {
             //TODO
         }
+    },
+
+    setLevel: function(level) {
+        defaultLevel = level;
     }
 }
 
